1a5a48ad21d956eb21dc123a044d434cbbae0e8c
tarotaron <suzu9095@gmail.com>
2020-03-19 12:19:17 +0000

    [add]adminのlength作成

---
+++ app/controllers/admin/lengths_controller.rb
@@ -0,0 +1,24 @@
+class Admin::LengthsController < ApplicationController
+	def index
+		@lengths = Length.all
+		@length = Length.new
+	end
+
+	def create
+		@length = Length.new(length_params)
+	    @length.save
+	    flash[:notice] = "You have creatad length."
+	    redirect_to  admin_lengths_path(@length)
+	end
+
+	def destroy
+		length = Length.find(params[:id])
+		length.destroy
+		redirect_to  admin_lengths_path
+	end
+
+private
+ def length_params
+ 	params.require(:length).permit(:length_name, :length_image)
+ end
+end
