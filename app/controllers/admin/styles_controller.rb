9002867169393306358b570ec8861c11b3366f7d
tarotaron <suzu9095@gmail.com>
2020-03-21 04:37:13 +0000

    [add]admin_style作成

---
+++ app/controllers/admin/styles_controller.rb
@@ -0,0 +1,30 @@
+class Admin::StylesController < ApplicationController
+
+	def index
+		@styles = Style.all
+		@style = Style.new
+	end
+
+	def create
+		@style = Style.new(style_params)
+		# binding.pry
+		if @style.save
+		flash[:notice] = "created style."
+		redirect_to 'index'
+	    else
+		@styles = Style.all
+	    render 'index'
+	    end
+	end
+
+	def destroy
+		style = Style.find(params[:id])
+		style.destroy
+		redirect_to admin_styles_path
+	end
+end
+
+private
+def style_params
+	params.require(:style).permit(:style_image, :style_body, :style_name, :length_id, :form_id)
+end
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,9 +1,9 @@
 class Style < ApplicationRecord
-	# belongs_to :length
-	# belongs_to :form
-	# attachment :style_image
+	belongs_to :length
+	belongs_to :form
+	attachment :style_image
 
-	# validates :style_image, presence: true
-	# validates :style_body, presence: true
-	# validates :style_name, presence: true
+	validates :style_image, presence: true
+	validates :style_body, presence: true
+	validates :style_name, presence: true
 end
