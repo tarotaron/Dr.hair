fe9ca2c1fbd65c3f06bc29ed1a59d708bb02d7dd
tarotaron <suzu9095@gmail.com>
2020-03-21 04:30:50 +0000

    [add]admiログイン設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
-  # devise_for :admins
-  # devise_for :users
-   devise_for :admins, controllers: {
-      sessions:      'admins/sessions',
-      passwords:     'admins/passwords',
-      registrations: 'admins/registrations'
-  }
-  devise_for :users, controllers: {
-      sessions:      'users/sessions',
-      passwords:     'users/passwords',
-      registrations: 'users/registrations'
-  }
+  devise_for :admins, path: :admin
+  devise_for :users
+  #  devise_for :admins, controllers: {
+  #     sessions:      'admins/sessions',
+  #     passwords:     'admins/passwords',
+  #     registrations: 'admins/registrations'
+  # }
+  # devise_for :users, controllers: {
+  #     sessions:      'users/sessions',
+  #     passwords:     'users/passwords',
+  #     registrations: 'users/registrations'
+  # }
 
   namespace :admin do
   	resources :forms, only:[:index, :create, :destroy]
   	resources :lengths, only:[:index, :create, :destroy]
+    resources :styles, only:[:index, :create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
