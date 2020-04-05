5f2ca24648d129de6eb0c71d0431444ab064e538
tarotaron <suzu9095@gmail.com>
2020-04-05 10:35:35 +0000

    [update]レイアウト修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,17 @@ $(".hover").hover(
     $(this).css('display', 'block');
   }
 );
+$(function() {
+function readURL(input) {
+    if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+$('#img_prev').attr('src', e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#icon").change(function(){
+    readURL(this);
+});
+});
